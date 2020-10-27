9de86c0f804725215d33164a6495588a9c6cdb56
Chetan Mann <chetanmann95@gmail.com>
2020-10-28 02:33:56 +0530

    Added push notification code with chat, receiving notifications properly

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component, useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer, StackActions, NavigationActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { fcmService } from './app/Notification/FCMService';
+import { localNotificationService } from './app/Notification/LocalNotificationService';
 
 
 import { StyleSheet, View, Text, StatusBar, Button } from 'react-native';
@@ -13,6 +15,38 @@ import ChatScreen from './app/component/chatScreen.js'
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  useEffect(()=>{
+    fcmService.registerAppWithFCM();
+    fcmService.register(onRegister, onNotification, onOpenNotification);
+    localNotificationService.configure(onOpenNotification)
+  },[])
+
+  const onRegister = (token) => {
+    console.log("[App] Token", token);
+  }
+
+  const onNotification = (notify) => {
+    // console.log("[App] onNotification", notify);
+    const options = {
+      soundName: 'default',
+      playSound: true,
+    }
+
+    localNotificationService.showNotification(
+      0,
+      notify.notification.title,
+      notify.notification.body,
+      notify,
+      options,
+    )
+  }
+
+  const onOpenNotification = async (notify) => {
+  
+    console.log('notify', notify);
+  }
+
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="white" barStyle={'dark-content'} />
